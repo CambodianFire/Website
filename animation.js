3199d0bdd8cacfb58605fdabdbc662b84ad6366e
CambodianFire <pkeo12@apu.edu>
2017-02-02 12:48:46 -0800

    Update animation.js

--- animation.js
+++ animation.js
@@ -2,3 +2,16 @@ function display(v1) {
 	var id = document.getElementById("v1");
 	id.style.display="none";
 }
+
+// Activate Carousel
+$("#description").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#description").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#description").carousel("prev");
+});
